49b0a2cf5b45082662be001dee614f209e9770ca
magabit <alejandragonzales.ayala@gmail.com>
2020-09-21 02:01:20 -0400

    Agregando api

---
+++ express-server/app.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var app = express();
+app.use(express.json());
+app.listen(3000, () => console.log("server running on port 3000"));
+
+app.get("/url", (req, res , next) => res.json(["Paris", "Barcelona", "Barranquilla", "Montevideo", "Santiago de Chile", "Nueva York"]));
+var misDestinos= [];
+app.get("/my", (req, res , next) => res.json(misDestinos));
+app.post("/my", (req, res , next) => {
+    console.log(req.body);
+    misDestinos = req.body;
+    res.json(misDestinos);
+});
